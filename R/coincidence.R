6b2834c31dd3a544e99cb15e275f716c571a4e7e
Karl Broman <kbroman@gmail.com>
2014-08-12 23:29:06 -0500

    Fix typo in help info for est.coi()

--- R/coincidence.R
+++ R/coincidence.R
@@ -1,16 +1,16 @@
 ## coincidence.R
 
 #' Estimate the coincidence function
-#' 
+#'
 #' Estimate the coincidence function from backcross data.
-#' 
+#'
 #' The coincidence function is the probability of a recombination event in both
-#' of two intervals, divided by the product of the two recombination fraction.
+#' of two intervals, divided by the product of the two recombination fractions.
 #' We estimate this as a function of the distance between the two intervals.
-#' 
+#'
 #' Note that we first call \code{\link[qtl]{fill.geno}} to impute any missing
 #' genotype data.
-#' 
+#'
 #' @param cross Cross object; must be a backcross.  See
 #' \code{\link[qtl]{read.cross}} for format details.
 #' @param chr Chromosome to consider (only one is allowed).  If missing, the
@@ -33,19 +33,19 @@
 #' genetic recombination.  \emph{Genetics} \bold{139}, 1031--1044.
 #' @keywords models
 #' @examples
-#' 
+#'
 #' map1 <- sim.map(103, n.mar=104, anchor=TRUE, include.x=FALSE, eq=TRUE)
 #' x <- sim.cross(map1, n.ind=2000, m=6, type="bc")
-#' 
+#'
 #' out <- est.coi(x, window=5)
 #' plot(coi1 ~ d, data=out, type="l", lwd=2, col="blue")
 #' lines(coi2 ~ d, data=out, lwd=2, col="green")
 #' lines(gammacoi(7), lwd=2, col="red", lty=2)
-#' 
+#'
 #' @useDynLib xoi
 #' @export
 est.coi <-
-function(cross, chr, pos, window=0, 
+function(cross, chr, pos, window=0,
          fill.method=c("imp", "argmax"), error.prob=1e-10,
          map.function=c("haldane", "kosambi", "c-f", "morgan"))
 {
@@ -58,7 +58,7 @@ function(cross, chr, pos, window=0,
     cross <- subset(cross, chr=chr)
   }
   else cross <- subset(cross, chr=1)
-    
+
   dat <- cross$geno[[1]]$data
   if(any(is.na(dat))) {
 
@@ -72,7 +72,7 @@ function(cross, chr, pos, window=0,
       dat[is.na(dat)] <- 0
     }
   }
-    
+
   map <- cross$geno[[1]]$map
   if(!missing(pos)) {
     if(length(pos) != length(map))
@@ -83,7 +83,7 @@ function(cross, chr, pos, window=0,
   ni <- nrow(dat)
   nm <- ncol(dat)
   if(nm < 3) stop("Need at least three markers.")
-  
+
   npair <- choose(nm-1, 2)
 
   out <- .C("R_est_coi",
@@ -94,11 +94,11 @@ function(cross, chr, pos, window=0,
             as.integer(dat),
             d=as.double(rep(0, npair)), # distances
             coi1=as.double(rep(0, npair)), # smooth top and bottom and then ratio
-            coi2=as.double(rep(0, npair)), # ratio then smooth 
+            coi2=as.double(rep(0, npair)), # ratio then smooth
             n=as.integer(0), # no. distances to keep
             as.double(window),
             PACKAGE="xoi")
-            
+
   n <- out$n
   data.frame(d=out$d[1:n], coi1=out$coi1[1:n], coi2=out$coi2[1:n])
 }
