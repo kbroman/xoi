06019d951a6884ffacc2373f5c2f39dca7f20596
Karl Broman <kbroman@gmail.com>
2014-09-23 14:32:26 -0500

    simStahl: default length now L=100
     - also change examples to be for chromosome of length 80 cM

--- R/simStahl.R
+++ R/simStahl.R
@@ -34,20 +34,25 @@
 #' @keywords datagen
 #' @examples
 #'
-#' # simulations with no interference
-#' xoNI <- simStahl(100, nu=1, p=0, L=200)
+#' # simulations with no interference, chromosome of length 80 cM
+#' xoNI <- simStahl(100, nu=1, p=0, L=80)
 #'
 #' # simulations under gamma model with nu=7.6
-#' xogamma <- simStahl(100, nu=7.6, p=0, L=200)
+#' xogamma <- simStahl(100, nu=7.6, p=0, L=80)
 #'
 #' # simulations under Stahl model with nu=7.6, p=0.1
-#' xostahl <- simStahl(100, nu=7.6, p=0.1, L=200)
+#' xostahl <- simStahl(100, nu=7.6, p=0.1, L=80)
 #'
+#' # simulations under chi-square model with nu=11 (m=10) and obligate chiasma
+#' xo_oblchi <- simStahl(100, nu=11, p=0, L=80, obligate_chiasma=TRUE)
+#'
+#' # simulations under Stahl model with nu=11, p=0.1, and obligate chiasma
+#' xo_oblchi_stahl <- simStahl(100, nu=11, p=0.1, L=80, obligate_chiasma=TRUE)
 #' @importFrom stats qpois
 #' @useDynLib xoi
 #' @export
 simStahl <-
-function(n.sim, nu=1, p=0, L=103,
+function(n.sim, nu=1, p=0, L=100,
          obligate_chiasma=FALSE, n.bins4start=10000)
 {
   if(nu <= 0) stop("nu should be positive.")
